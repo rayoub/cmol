d8e46e84af1f6d4470869a99eb8ded5d2e44e452
Ronald Ayoub <ronaldayoub@outlook.com>
2021-10-05 11:02:56 -0500

    update

---
+++ NGS-QciDb/Db/x_create_all.sql
@@ -0,0 +1,13 @@
+
+\i qci_report.sql
+\i qci_variant.sql
+
+\i qci_truncate.sql
+
+\i expand_acronym.sql
+\i sentence_case.sql
+
+\i insert_qci_report.sql
+\i insert_qci_variant.sql
+
+\i get_qci_report_ids.sql
