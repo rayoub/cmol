8d24f6906e717ce81a0ca2048c95a16da0288926
Ronald Ayoub <ronaldayoub@outlook.com>
2021-10-22 10:05:02 -0500

    sources

--- NGS-QciDb/Db/x_clean_pts.sql
+++ NGS-QciDb/Db/x_clean_pts.sql
@@ -9,6 +9,15 @@
 --ORDER BY
 --    primary_tumor_site;
 
+-- only one type of null
+UPDATE 
+    qci_report 
+SET
+    primary_tumor_site = 'Not Provided'
+WHERE
+    primary_tumor_site = ''
+    OR primary_tumor_site IS NULL;
+
 -- title case everything that is not all caps
 UPDATE
     qci_report
@@ -16,3 +25,4 @@ SET
     primary_tumor_site = INITCAP(primary_tumor_site)
 WHERE
     primary_tumor_site <> UPPER(primary_tumor_site);
+
