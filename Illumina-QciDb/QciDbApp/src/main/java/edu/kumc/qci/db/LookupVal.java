dbb6d24b351dd1eecf2a3c9bff2fbcafecaa9e6e
Ronald Ayoub <ronaldayoub@gmail.com>
2022-05-11 09:07:54 -0500

    update

---
+++ Illumina-QciDb/QciDbApp/src/main/java/edu/kumc/qci/db/LookupVal.java
@@ -0,0 +1,23 @@
+package edu.kumc.qci.db;
+
+public class LookupVal {
+    
+    private int id;
+    private String descr;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescr() {
+        return descr;
+    }
+
+    public void setDescr(String descr) {
+        this.descr = descr;
+    }
+}
