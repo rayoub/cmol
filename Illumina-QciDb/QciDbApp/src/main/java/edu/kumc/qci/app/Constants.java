4d333ad9d836132f85cd21ef9abdc4a9f7e73214
rayoub <ronaldayoub@gmail.com>
2022-07-05 06:56:55 -0500

    refresh

--- Illumina-QciDb/QciDbApp/src/main/java/edu/kumc/qci/app/Constants.java
+++ Illumina-QciDb/QciDbApp/src/main/java/edu/kumc/qci/app/Constants.java
@@ -6,5 +6,5 @@ public class Constants {
     public static String DB_NAME = "cmol";
     public static String DB_USER = "postgres";
     public static String DB_PASSWORD = "postgres";
-    public static String DATA_PATH = "D:/Code/cmol/Illumina-QciDb/Data";
+    public static String DATA_PATH = "E:/git/cmol/Illumina-QciDb/Data";
 }
