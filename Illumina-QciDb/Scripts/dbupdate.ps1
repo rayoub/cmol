c31b1022bba6de03084fd21aaeb8b0d86e58388e
rayoub <ronaldayoub@gmail.com>
2022-08-24 08:43:46 -0500

    script

---
+++ Illumina-QciDb/Scripts/dbupdate.ps1
@@ -0,0 +1,4 @@
+
+java -jar E:\git\cmol\Illumina-QciDb\QciDbApp\target\QciDbApp-0.0.1-SNAPSHOT-jar-with-dependencies.jar -g
+java -jar E:\git\cmol\Illumina-QciDb\QciDbApp\target\QciDbApp-0.0.1-SNAPSHOT-jar-with-dependencies.jar -i
+java -jar E:\git\cmol\C3od\C3odApp\target\C3odApp-0.0.1-SNAPSHOT-jar-with-dependencies.jar -p 
