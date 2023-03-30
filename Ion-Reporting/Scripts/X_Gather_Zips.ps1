550681d71f74ef018f310ecb29bba50a43726bbe
rayoub <ronaldayoub@gmail.com>
2023-03-30 12:29:39 -0500

    commit

--- Ion-Reporting/Scripts/X_Gather_Zips.ps1
+++ Ion-Reporting/Scripts/X_Gather_Zips.ps1
@@ -1,23 +1,45 @@
 
-$root = "\\kumc.edu\data\Research\CANCTR RSCH\CMOL\Assay Results\Clinical Data\NGS Comprehensive Plus\"
-$paths = 
-    ($root + "2022 (Run 001-015)"),
-    ($root + "2023 (Run 016- )")
+# set locations 
+$root = "R:\CMOL\Assay Results\Clinical Data\NGS Comprehensive Plus\"
+$paths = @(($root + "2022 (Run 001-015)"), ($root + "2023 (Run 016- )"))
+$data = "E:\git\cmol\Cmol-Db\Data\Ion"
 
-$data = "E:\git\cmol\Cmol-Db\Data"
-
+# get zip files
 $files = @()
 foreach($path in $paths) {
-
     $files += Get-ChildItem -Path $path -Filter CP*.zip -Recurse -Depth 2
 }
+
+# create zip objects
 $zips = @($null) * $files.Length
 for($i = 0; $i -lt $files.Length; $i++){
     $zips[$i] = [PSCustomObject]@{ 
+        DirectoryName = $files[$i].DirectoryName;
         FileName = $files[$i].Name;
         SampleFolder = $files[$i].Directory.Name;
         AssayFolder = $files[$i].Directory.Parent.Name
     }
 }
 
-$zips = $zips | Sort-Object -Property AssayFolder,SampleFolder,FileName | Select-Object -Property AssayFolder,SampleFolder,FileName
+# sort zip objects
+$zips = $zips | Sort-Object -Property AssayFolder,SampleFolder,DirectoryName,FileName | Select-Object -Property AssayFolder,SampleFolder,DirectoryName,FileName
+
+# iterate zip objects
+foreach($zip in $zips) {
+
+    Write-Host "Processing a zip file for" $zip.SampleFolder 
+
+    $shell = New-Object -com shell.application
+
+    $variantsFolder = $zip.DirectoryName + "\" + $zip.FileName + "\Variants"
+    $tsvFolder = $shell.namespace($variantsFolder).items() | Select-Object -First 1
+    $tsvFile = $shell.namespace($variantsFolder + "\" + $tsvFolder.Name).items() | Where-Object { $_.Name -like "*full.tsv"} | Select-Object -First 1
+    
+    Write-Host "Extracting" $tsvFile.Name
+    $shell.namespace($data).copyhere($tsvFile, 16)
+  
+    $tsvFullName = $data + "\" + $tsvFile.Name
+    $tsvNewFullName = $data + "\" + "(" + $zip.AssayFolder + ")(" + $zip.SampleFolder + ")(" + $zip.FileName + ")" + [io.path]::GetFileNameWithoutExtension($tsvFile.Name) + ".tsv"
+
+    Rename-Item -Path $tsvFullName -NewName $tsvNewFullName
+}
