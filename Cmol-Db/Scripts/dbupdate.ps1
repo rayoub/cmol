c1d9e95ce53b88df222a6bd69591dc6c1fb3e5cc
rayoub <ronaldayoub@gmail.com>
2023-06-14 10:22:56 -0500

    commit

--- Cmol-Db/Scripts/dbupdate.ps1
+++ Cmol-Db/Scripts/dbupdate.ps1
@@ -2,4 +2,11 @@
 java -jar E:\git\cmol\Cmol-Db\CmolDbApp\target\CmolDbApp-0.0.1-SNAPSHOT-jar-with-dependencies.jar -q
 java -jar E:\git\cmol\Cmol-Db\CmolDbApp\target\CmolDbApp-0.0.1-SNAPSHOT-jar-with-dependencies.jar -r
 java -jar E:\git\cmol\Cmol-Db\CmolDbApp\target\CmolDbApp-0.0.1-SNAPSHOT-jar-with-dependencies.jar -s
-java -jar E:\git\cmol\C3od\C3odApp\target\C3odApp-0.0.1-SNAPSHOT-jar-with-dependencies.jar -p 
+E:\git\cmol\Cmol-Db\Scripts\ion_gather_mrns.ps1
+E:\git\cmol\Cmol-Db\Scripts\ion_gather_tsvs.ps1
+
+
+
+
+
+
