e17f8c5baed53ee828a53daf9fc888d96b4adced
rayoub <ronaldayoub@gmail.com>
2023-09-22 07:58:39 -0500

    commit

--- Cmol-Db/Scripts/ion_gather_tsvs.ps1
+++ Cmol-Db/Scripts/ion_gather_tsvs.ps1
@@ -4,13 +4,6 @@ $root = "R:\CMOL\Assay Results\Clinical Data\NGS Comprehensive Plus\"
 $paths = @(($root + "2022 (Run 001-015)"), ($root + "2023 (Run 016- )"))
 $data = "E:\git\cmol\Cmol-Db\Data\Ion"
 
-# assays we already did
-$assayFolders = New-Object System.Collections.Generic.HashSet[String]
-$existingTsvs = Get-ChildItem -Path $data -Filter *.tsv 
-foreach($existingTsv in $existingTsvs) {
-    $assayFolders.Add(($existingTsv.Name -split ' ')[0]) | Out-Null
-}
-
 # get zip files
 $files = @()
 foreach($path in $paths) {
@@ -31,6 +24,13 @@ for($i = 0; $i -lt $files.Length; $i++){
 # sort zip objects
 $zips = $zips | Sort-Object -Property AssayFolder,SampleFolder,DirectoryName,FileName | Select-Object -Property AssayFolder,SampleFolder,DirectoryName,FileName
 
+# assays we already did
+$existingTsvs = Get-ChildItem -Path $data -Filter *.tsv 
+$assayFolders = New-Object System.Collections.Generic.HashSet[String]
+foreach($existingTsv in $existingTsvs) {
+    $assayFolders.Add(($existingTsv.Name -split ' ')[0]) | Out-Null
+}
+
 # iterate zip objects
 foreach($zip in $zips) {
 
@@ -42,14 +42,18 @@ foreach($zip in $zips) {
 
         $variantsFolder = $zip.DirectoryName + "\" + $zip.FileName + "\Variants"
         $tsvFolder = $shell.namespace($variantsFolder).items() | Select-Object -First 1
-        $tsvFile = $shell.namespace($variantsFolder + "\" + $tsvFolder.Name).items() | Where-Object { $_.Name -like "*full.tsv"} | Select-Object -First 1
-        
-        Write-Host "Extracting" $tsvFile.Name
-        $shell.namespace($data).copyhere($tsvFile, 16)
-    
-        $tsvFullName = $data + "\" + $tsvFile.Name
-        $tsvNewFullName = $data + "\" + $zip.AssayFolder + " " + $zip.SampleFolder + " " + [io.path]::GetFileNameWithoutExtension($zip.FileName) + " " + $tsvFile.Name
+        $tsvFiles = $shell.namespace($variantsFolder + "\" + $tsvFolder.Name).items() | Where-Object { $_.Name -like "*.tsv"}
+       
+        foreach($tsvFile in $tsvFiles) {
 
-        Rename-Item -Path $tsvFullName -NewName $tsvNewFullName
+            $tsvFileName = $tsvFile.Name.replace(":","_")
+            Write-Host "Extracting" $tsvFileName
+            $shell.namespace($data).copyhere($tsvFile, 16) | Out-Null
+        
+            $tsvFullName = $data + "\" + $tsvFileName
+            $tsvNewFullName = $data + "\" + $zip.AssayFolder + " " + $zip.SampleFolder + " " + [io.path]::GetFileNameWithoutExtension($zip.FileName) + " " + $tsvFileName
+
+            Rename-Item -Path $tsvFullName -NewName $tsvNewFullName
+        }
     }
 }
