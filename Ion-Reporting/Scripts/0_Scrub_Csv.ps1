40467663fd59a522c8529032e64d8677f05f7e9f
rayoub <ronaldayoub@gmail.com>
2024-11-15 07:38:14 -0600

    commit

---
+++ Ion-Reporting/Scripts/0_Scrub_Csv.ps1
@@ -0,0 +1,23 @@
+
+Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName System.Drawing
+
+function Get-FileName
+{
+    $OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
+    $OpenFileDialog.InitialDirectory = $PSScriptRoot
+    $OpenFileDialog.filter = "Comma delimited (*.csv) | *.csv"
+    $OpenFileDialog.Title = "Select CSV file to scrub"
+    $OpenFileDialog.ShowDialog() | Out-Null
+    $OpenFileDialog.FileName
+}
+
+$file = Get-FileName
+
+(Get-Content $file) `
+	-replace ',(Patient Name:)', ',"$1' `
+	-replace ',(MRN:)','",$1' `
+	-replace ',(Authorizing Provider:)', ',"$1' `
+	-replace ',(Ordering Provider:)','","$1' `
+	-replace ', (Facility:)','",$1' | 
+	Set-Content $file
