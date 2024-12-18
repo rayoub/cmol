113b71bda2a6638a547573104547311f3368ddc5
rayoub <ronaldayoub@gmail.com>
2024-12-18 15:53:44 -0600

    commit

--- Cmol-Db/Scripts/dbupdate.ps1
+++ Cmol-Db/Scripts/dbupdate.ps1
@@ -1,18 +1,17 @@
-
-Write-Host "geting QCI XML files"
-java -jar E:\git\cmol\Cmol-Db\CmolDbApp\target\CmolDbApp-0.0.1-SNAPSHOT-jar-with-dependencies.jar -q
-Write-Host "importing QCI XML files"
+Write-Host "Importing Lab loose files"
+java -jar E:\git\cmol\Cmol-Db\CmolDbApp\target\CmolDbApp-0.0.1-SNAPSHOT-jar-with-dependencies.jar -b
+Write-Host "Importing QCI XML files"
 java -jar E:\git\cmol\Cmol-Db\CmolDbApp\target\CmolDbApp-0.0.1-SNAPSHOT-jar-with-dependencies.jar -r
-Write-Host "cleaning QCI tables"
+Write-Host "Cleaning QCI tables"
 java -jar E:\git\cmol\Cmol-Db\CmolDbApp\target\CmolDbApp-0.0.1-SNAPSHOT-jar-with-dependencies.jar -s
 E:\git\cmol\Cmol-Db\Scripts\ion_gather_mrns.ps1
 E:\git\cmol\Cmol-Db\Scripts\ion_gather_tsvs.ps1
 E:\git\cmol\Cmol-Db\Scripts\ion_gather_vcfs.ps1
-Write-Host "importing Ion selected variants"
+Write-Host "Importing Ion selected variants"
 java -jar E:\git\cmol\Cmol-Db\CmolDbApp\target\CmolDbApp-0.0.1-SNAPSHOT-jar-with-dependencies.jar -i
-Write-Host "importing Ion filtered variants"
+Write-Host "Importing Ion filtered variants"
 java -jar E:\git\cmol\Cmol-Db\CmolDbApp\target\CmolDbApp-0.0.1-SNAPSHOT-jar-with-dependencies.jar -j
-Write-Host "importing Ion MRNs"
+Write-Host "Importing Ion MRNs"
 java -jar E:\git\cmol\Cmol-Db\CmolDbApp\target\CmolDbApp-0.0.1-SNAPSHOT-jar-with-dependencies.jar -k
 
 
